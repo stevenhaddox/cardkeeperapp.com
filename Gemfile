5061a428d8ab9ca00e17b8420023944c027e98e8
Steven Haddox <steven@haddox.us>
2011-07-28 08:52:41 -0400

    initial Rails commit

---
+++ Gemfile
@@ -0,0 +1,15 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.0.9'
+gem 'sqlite3'
+gem 'heroku'
+
+gem 'high_voltage'
+gem 'flash-message-conductor'
+
+group :development, :test do
+  gem 'rspec-rails'
+  gem 'cucumber-rails'
+  gem 'factory_girl_rails'
+  gem 'database_cleaner'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CardkeeperappCom::Application.config.secret_token = '9b35d43f63d5729df9b4023ecf9cee18f05c03c361a751b5079f225372365b0e111ac1075f791ba3aee317767f5e878aaa000606e8835ba55a1b320222b3e513'
